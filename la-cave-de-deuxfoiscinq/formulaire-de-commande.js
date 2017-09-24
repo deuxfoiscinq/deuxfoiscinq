39452366dc4a71396a298c019db2f3ee80ffb0d0
NicHub <nicolas.jeanmonod@gmail.com>
2017-09-24 22:55:40 +0200

    formulaire fonctionnel

--- la-cave-de-deuxfoiscinq/formulaire-de-commande.js
+++ la-cave-de-deuxfoiscinq/formulaire-de-commande.js
@@ -81,31 +81,23 @@ function displayJSONinHTML()
         let backColor = "white";
         if( j % 2 ) backColor = "#EEE";
         cptVins += 1;
-        let nomInput = vin.PRIX;
+        let nomInput = vin.REFERENCE + " | " + vin.PRIX;
 
         htmlElems += `
           <div class="row" style="margin-top:5px; background-color: ${ backColor };">
-            <div class="col-xs-10">
+            <div class="col-xs-8 col-sm-10">
               <div class="row">
-                <div class="row">
-                  <div class="col-xs-4"><em>${ vin.CEPAGE }</em></div>
-                  <div class="col-xs-4"><em>${ vin.NOM }</em></div>
-                  <div class="col-xs-4">${ vin.CEPAGE_2 }</div>
-                </div>
-              </div>
-              <div class="row">
-                <div class="row">
-                  <div class="col-xs-4">${ vin.REGION }</div>
-                  <div class="col-xs-2">${ vin.ANNEE }</div>
-                  <div class="col-xs-2">${ vin.COULEUR }</div>
-                  <div class="col-xs-4">${ vin.PRIX } CHF</div>
-                </div>
-                <div class="row">
-                  <div class="col-xs-12"><small class="text-muted">${ aoc } ${ vin.INFORMATIONS_COMPLEMENTAIRES }&nbsp;</small></div>
-                </div>
+                  <div class="col-xs-6                 col-sm-4                "> <em>${ vin.CEPAGE }</em></div>
+                  <div class="col-xs-6                 col-sm-4                "> <em>${ vin.NOM }</em></div>
+                  <div class="col-xs-6                 col-sm-4                "> ${ vin.CEPAGE_2 }</div>
+                  <div class="col-xs-6                 col-sm-4                "> ${ vin.REGION }</div>
+                  <div class="col-xs-6                 col-sm-2                "> ${ vin.ANNEE }</div>
+                  <div class="col-xs-6                 col-sm-2                "> ${ vin.COULEUR }</div>
+                  <div class="col-xs-6 col-xs-offset-6 col-sm-4 col-sm-offset-0"> ${ vin.PRIX } CHF</div>
+                  <div class="col-xs-6                 col-sm-12               "  > <small class="text-muted">${ aoc } ${ vin.INFORMATIONS_COMPLEMENTAIRES }&nbsp;</small></div>
               </div>
             </div>
-            <div class="col-xs-2" style="margin-top:15px">
+            <div class="col-xs-4 col-sm-2" style="margin-top:15px">
                 <input name="${ nomInput }" type="number" min="0" class="text-center cptBouteilles" onchange="calculTotal()" />
             </div>
           </div>
@@ -115,16 +107,16 @@ function displayJSONinHTML()
 
     htmlElems +=`
     <div class="row">
-      <div class="col-xs-10 text-right">total des bouteilles commandées (CHF)</div>
-      <div class="col-xs-2 text-center"><p style="padding-right:30px;" id="total1" class="text-right">0.00</p></div>
+      <div class="col-xs-8  col-sm-10 text-right">total des bouteilles commandées (CHF)</div>
+      <div class="col-xs-4  col-sm-2  text-center"><p style="padding-right:30px;" id="total1" class="text-right">0.00</p></div>
     </div>
     <div class="row">
-      <div class="col-xs-10 text-right">frais d’emballage et de livraison (CHF)</div>
-      <div class="col-xs-2 text-right"><p style="padding-right:30px;">${ fraisDePort.toFixed( 2 ) }</p></div>
+      <div class="col-xs-8  col-sm-10 text-right">frais d’emballage et de livraison (CHF)</div>
+      <div class="col-xs-4  col-sm-2  text-right"><p style="padding-right:30px;">${ fraisDePort.toFixed( 2 ) }</p></div>
     </div>
     <div class="row">
-      <div class="col-xs-10 text-right"><strong>total avec TVA 8% incluse (CHF)</strong></div>
-      <div class="col-xs-2 text-right"><p style="padding-right:30px;" id="total2"><strong>0.00</strong></p></div>
+      <div class="col-xs-8  col-sm-10 text-right"><strong>total avec TVA 8% incluse&nbsp;(CHF)</strong></div>
+      <div class="col-xs-4  col-sm-2  text-right"><p style="padding-right:30px;" id="total2"><strong>0.00</strong></p></div>
     </div>
 
     <div class="row" style="margin-top:30px;">
@@ -137,6 +129,8 @@ function displayJSONinHTML()
         <p style="margin:0px; line-height: 15px">panachage possible</p>
         <p style="margin:0px; line-height: 15px">les prix sont indiqués en CHF et TVA 8% incluse</p>
         <p style="margin:0px; line-height: 15px">frais d’emballage et de livraison ${ fraisDePort } CHF</p>
+        <p style="margin:0px; line-height: 15px">les marchandises vendues ne sont ni reprises ni échangées</p>
+        <p style="margin:0px; line-height: 15px">le for juridique pour toute réclamation en relation avec votre commande est Lausanne en Suisse</p>
       </div>
     </div>
 
@@ -162,14 +156,14 @@ function displayJSONinHTML()
       </div>
       <div class="row" style="margin-top:5px;">
         <div class="col-sm-4 col-xs-12" style="margin: 0px;"><label>adresse email</label></div>
-        <div class="col-sm-8 col-xs-12" style="margin: 0px;"><input name="adresse-livraison-mail" type="email" class="" /></div>
+        <div class="col-sm-8 col-xs-12" style="margin: 0px;"><input name="_replyto" type="email" class="" /></div>
       </div>
     </div>
 
     <div class="row" style="margin-top:30px;">
       <div class="row" style="margin:15px 0;">
         <div class="col-xs-12" style="border-bottom: 4px solid;"><h3>adresse de facturation</h3></div>
-        <div class="col-xs-12" style="margin: 12px 0 4px"><p><small class="text-muted">uniquement si différente de l’adresse de livraison</small></p></div>
+        <div class="col-xs-12" style="margin: 12px 0 4px"><p><small class="text-muted">à remplir uniquement si l’adresse de facturation différente de&nbsp;l’adresse de livraison</small></p></div>
       </div>
       <div class="row" style="margin-top:5px;">
         <div class="col-sm-4 col-xs-12" style="margin: 0px;"><label>nom et prénom</label></div>
@@ -199,12 +193,17 @@ function displayJSONinHTML()
     </div>
 
     <div class="row" style="margin-top:60px;">
+
+      <div class="col-xs-12" style="margin: 12px 0 4px"><p><small class="text-muted">je vous enverrai un email de confirmation de réception de votre commande dans les 24 h ouvrables</small></p></div>
       <div class="col-sm-8 col-sm-offset-2 col-xs-10 col-xs-offset-1" style="margin-top: 3px;">
         <button type="submit" class="btn btn-block" style="height:50px;">ENVOYER LA COMMANDE</button>
       </div>
     </div>
 
-
+    <input type="text" name="_format" value="plain" style="display:none" />
+    <input type="hidden" name="_language" value="fr" />
+    <input type="text" name="_gotcha" style="display:none" />
+    <input type="hidden" name="_cc" value="&#110;&#106;&#100;&#064;&#098;&#108;&#117;&#101;&#119;&#105;&#110;&#046;&#099;&#104;,&#106;&#105;&#115;&#115;&#101;&#064;&#100;&#101;&#117;&#120;&#102;&#111;&#105;&#115;&#099;&#105;&#110;&#113;&#046;&#099;&#104;" />
     </div> <!-- .container-fluid -->
     `;
     htmlTarget.innerHTML = htmlElems;
@@ -225,7 +224,8 @@ function calculTotal()
   let prixTotal = 0;
   for( let i=0; i<bouteilles.length; i++ )
   {
-    prixTotal += bouteilles[ i ].name * bouteilles[ i ].value;
+    let prix = bouteilles[ i ].name.split( " | " )[ 1 ];
+    prixTotal += prix * bouteilles[ i ].value;
   }
   total1.innerHTML = prixTotal.toFixed( 2 );
   total2.innerHTML = (prixTotal + fraisDePort).toFixed( 2 );;
